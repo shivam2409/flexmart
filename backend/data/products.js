694aa69327dc899756fbd27fba14c45ea23655f5
Shivam Patel <51165698+shivam2409@users.noreply.github.com>
2021-01-28 20:02:05 -0500

    creating backend

---
+++ backend/data/products.js
@@ -0,0 +1,41 @@
+const products = [
+  {
+    _id: '1',
+    name: 'iPhone',
+    image: '/img/iphone.jpg',
+
+    description: 'Apples most latest product',
+    brand: 'apple',
+    catagory: 'Electronics',
+    price: 889.99,
+    countInStock: 0,
+    rating: 2.5,
+    numReviews: 4,
+  },
+  {
+    _id: '2',
+    name: 'iPhone',
+    image: '/img/iphone.jpg',
+    description: 'Apples most latest product',
+    brand: 'apple',
+    catagory: 'Electronics',
+    price: 889.99,
+    countInStock: 3,
+    rating: 3.5,
+    numReviews: 4,
+  },
+  {
+    _id: '3',
+    name: 'iPhone',
+    image: '/img/iphone.jpg',
+    description: 'Apples most latest product',
+    brand: 'apple',
+    catagory: 'Electronics',
+    price: 889.99,
+    countInStock: 3,
+    rating: 5,
+    numReviews: 4,
+  },
+];
+
+module.exports = products;
