3789b09d439d39239a8b649c9a465ec8fef9d8ab
Shivam Patel <51165698+shivam2409@users.noreply.github.com>
2021-02-01 23:46:35 -0500

    seeder for product

--- backend/data/users.js
+++ backend/data/users.js
@@ -1,22 +1,22 @@
-import bcrypt, { hash } from 'bcryptjs';
+import bcrypt from 'bcryptjs';
 
 const users = [
   {
     name: 'Admin user',
     email: 'admin@test.com',
-    password: bcrypt.hashSync('123456', 10  ),
+    password: bcrypt.hashSync('123456', 10),
     isAdmin: true,
   },
   {
     name: 'Shivam Patel',
     email: 'shivam@test.com',
-    password: bcrypt.hashSync('123456', 10  ),
+    password: bcrypt.hashSync('123456', 10),
+  },
   {
     name: 'Devi Shah',
     email: 'devi@test.com',
-    password: bcrypt.hashSync('123456', 10  ),
+    password: bcrypt.hashSync('123456', 10),
   },
 ];
 
-
-export default users
+export default users;
