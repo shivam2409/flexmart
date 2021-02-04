dd2c7a8ca565c119b73d02d0d3a98087d8464973
Shivam Patel <51165698+shivam2409@users.noreply.github.com>
2021-02-03 19:53:05 -0500

    middleware

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import colors from 'colors';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes';
+import { notFound, errorHandler } from './middleware/errorMiddleware';
 
 dotenv.config();
 
@@ -14,6 +15,10 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/products', productRoutes);
+
+app.use(notFound);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
