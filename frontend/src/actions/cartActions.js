2616c799d0bdc6e46b41ffce9c4d707fa63fb850
Shivam Patel <51165698+shivam2409@users.noreply.github.com>
2021-02-08 22:49:31 -0500

    cart action and reducer

---
+++ frontend/src/actions/cartActions.js
@@ -0,0 +1,18 @@
+import { CART_ADD_ITEM } from '../constants/cartConstants';
+
+export const addToCart = (id, qty) => async (dispatch, getState) => {
+  const { data } = await axios.get(`/api//products/${id}`);
+
+  dispatch({
+    type: CART_ADD_ITEM,
+    payload: {
+      Product: data.id,
+      name: data.name,
+      image: data.image,
+      price: data.price,
+      countInStock: data.countInStock,
+      qty,
+    },
+  });
+  localStorage.setItem('cartitems', JSON.stringify(getState().cart.cartItems));
+};
---
+++ frontend/src/reducers/cartReducers.js
@@ -0,0 +1,26 @@
+import { CART_ADD_ITEM } from '../constants/cartConstants';
+
+export const cartReducer = (state = { cartItems: [] }, action) => {
+  switch (action.type) {
+    case CART_ADD_ITEM:
+      const item = action.payload;
+      const existItem = state.cartItems.find((p) => p.product === item.product);
+
+      if (existItem) {
+        return {
+          ...state,
+          cartItem: state.cartItems.map((p) =>
+            p.product === existItem.product ? item : p
+          ),
+        };
+      } else {
+        return {
+          ...state,
+          cartItem: [...state.cartItems, item],
+        };
+      }
+
+    default:
+      return state;
+  }
+};
