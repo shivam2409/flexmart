5fcdec9fb963daac7a7e3a4c77e768bd91936529
Shivam Patel <51165698+shivam2409@users.noreply.github.com>
2021-03-07 20:07:09 -0500

    user login constants,reducers,actions

--- frontend/src/reducers/productReducer.js
+++ frontend/src/reducers/productReducer.js
@@ -30,7 +30,7 @@ export const productDetailsReducer = (
     case PRODUCT_DETAILS_SUCCESS:
       return { loading: false, product: action.payload };
     case PRODUCT_DETAILS_FAIL:
-      return { loading: true, error: action.payload };
+      return { loading: false, error: action.payload };
     default:
       return state;
   }
