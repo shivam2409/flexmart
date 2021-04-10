0ed6c34042bdb2f8f9fac1b29ddf3826d44d5894
Shivam Patel <51165698+shivam2409@users.noreply.github.com>
2021-04-09 20:32:18 -0400

    orders in profile

--- frontend/src/sceens/ProfileScreen.js
+++ frontend/src/sceens/ProfileScreen.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { Col, Form, Row, Button } from 'react-bootstrap';
+import { Col, Form, Row, Button, Table } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import { Loader } from '../components/Loader';
 import Message from '../components/Message';
@@ -25,7 +26,7 @@ const ProfileScreen = ({ history }) => {
   const { success } = userUpdateProfile;
 
   const orderMyList = useSelector((state) => state.orderMyList);
-  const { loading: loadingOrders, error: errororders } = orderMyList;
+  const { loading: loadingOrders, error: errororders, orders } = orderMyList;
 
   useEffect(() => {
     if (!userInfo) {
@@ -110,7 +111,55 @@ const ProfileScreen = ({ history }) => {
           </Button>
         </Form>
       </Col>
-      <Col md={9}></Col>
+      <Col md={9}>
+        <h2>My Orders</h2>
+        {loadingOrders ? (
+          <Loader />
+        ) : errororders ? (
+          <Message varient='danger'>{errororders}</Message>
+        ) : (
+          <Table striped bordered hover responsive className='table-sm'>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>DATE</th>
+                <th>TOTAL </th>
+                <th>PAID</th>
+                <th>DELIVERED</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {orders.map((order) => (
+                <tr key={order._id}>
+                  <td>{order._id}</td>
+                  <td>{order.createdAt.substring(0, 10)}</td>
+                  <td>{order.totalPrice}</td>
+                  <td>
+                    {order.isPaid ? (
+                      order.paidAt.substring(0, 10)
+                    ) : (
+                      <i className='fas fa-times'></i>
+                    )}
+                  </td>
+                  <td>
+                    {order.isDelivered ? (
+                      order.deliveredAt.substring(0, 10)
+                    ) : (
+                      <i className='fas fa-times'></i>
+                    )}
+                  </td>
+                  <td>
+                    <LinkContainer to={`/order/${order._id}`}>
+                      <button varient='light'>Details</button>
+                    </LinkContainer>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        )}
+      </Col>
     </Row>
   );
 };
