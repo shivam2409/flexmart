c6838403dd26199336497d10a9002f6a535ded5e
Shivam Patel <51165698+shivam2409@users.noreply.github.com>
2021-06-12 14:06:16 -0400

    create and update Product endpoints

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -25,7 +25,7 @@ const getProductById = asyncHandler(async (req, res) => {
   }
 });
 
-// @des Delete a products
+// @desc Delete a products
 // @route DELETE /api/products
 // @access Private/admin
 
@@ -39,4 +39,57 @@ const deleteProduct = asyncHandler(async (req, res) => {
   }
 });
 
-export { getProducts, getProductById, deleteProduct };
+// @desc Create a products
+// @route POST /api/products
+// @access Private/admin
+
+const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product({
+    name: 'Sample name',
+    price: 0,
+    user: req.user._id,
+    image: '/images/sample.jpg',
+    brand: 'Sample brand',
+    category: 'Sample cate',
+    countInStocks: 0,
+    numReviews: 0,
+    description: 'Sample desc',
+  });
+
+  const createdProduct = await product.save();
+  res.status(201).json(createdProduct);
+});
+
+// @desc Update a products
+// @route PUT /api/products/:id
+// @access Private/admin
+
+const updateProduct = asyncHandler(async (req, res) => {
+  const { name, price, description, image, brand, category, countInStocks } =
+    req.body;
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    product.name = name;
+    product.price = price;
+    product.description = description;
+    product.image = image;
+    product.brand = brand;
+    product.category = category;
+    product.countInStocks = countInStocks;
+
+    const updatedProduct = await product.save();
+    res.json(updatedProduct);
+  } else {
+    res.status(404);
+    throw new Error('Product not found');
+  }
+});
+
+export {
+  getProducts,
+  getProductById,
+  deleteProduct,
+  updateProduct,
+  createProduct,
+};
