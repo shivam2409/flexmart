893fc6e8726721579e019b24e2b80cf2d8cf1792
Shivam Patel <51165698+shivam2409@users.noreply.github.com>
2021-06-13 15:25:14 -0400

    admin product

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,6 +2,7 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import {
+  productCreateReducer,
   productDetailsReducer,
   productListReducer,
 } from './reducers/productReducer';
@@ -35,6 +36,7 @@ const reducer = combineReducers({
   orderDetails: orderDetailsReducer,
   orderPay: orderPayReducer,
   orderMyList: ordersListMyReducer,
+  productCreate: productCreateReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
